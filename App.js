a5decdeb89690f9e4ded5fe77654909f3ab6f582
Oliver Pile <oliver.pile@bt.com>
2023-11-23 11:38:10 +0000

    Add styles to counter

--- App.js
+++ App.js
@@ -1,23 +1,32 @@
 import { StyleSheet,Text, View, SafeAreaView, Button } from 'react-native';
 import React, { useState } from 'react';
+import { inline } from 'react-native-web/dist/cjs/exports/StyleSheet/compiler';
 
 export default function App() {
   const [count, setCount] = useState(0);
 
-  const increaseCount = () => {
-    setCount(count + 1);
+  const increaseCount = (by) => {
+    setCount(count + by);
     };
 
-  
-    const resetCount = () => {
-      setCount(0);
-      };
+  const decrementCount = () => {
+    setCount(count - 1);
+    };
+
+
+  const resetCount = () => {
+    setCount(0);
+    };
     
   return (
     <SafeAreaView style={styles.container}>
-      <Text>{count}</Text>
-      <Button onPress={increaseCount} title="+1" />
-      <Button onPress={resetCount} title="Reset" />
+      <Text style={styles.count}>{count}</Text>
+      <View style={styles.row}>
+        <Button color="red" onPress={decrementCount} title="-1" />
+        <Button color="green" onPress={() => increaseCount(1)} title="+1" />
+      </View>
+      <Button color="orange" onPress={() => increaseCount(10)} title="Bonus" />
+      <Button color="purple" onPress={resetCount} title="Reset" />
     </SafeAreaView>
   );
 
@@ -27,8 +36,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#3498db',
+    color: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  count: {
+    fontSize: 50,
+    marginBottom: 40
+  },
+  row: {
+    flexDirection: "row",
+    columnGap: 20,
+    marginBottom: 20
+  }
 });
